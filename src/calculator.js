8618dc80c4d07d681c9e2049c14052c78f4a0102
Simon Vegsund <sve034@uit.no>
2024-10-30 13:18:11 +0100

    calculations with chaining operators is not working

--- src/calculator.js
+++ src/calculator.js
@@ -4,6 +4,9 @@ export class Calculator {
 		this.secondOperand = document.querySelector('.secondary-operand');
 		this.result = 0;
 		this.currentOperator = '';
+		this.numberArray = [];
+		this.resultArray = [];
+		this.regEx = /[*+-÷]/;
 	}
 
 	addNumbers() {
@@ -34,7 +37,7 @@ export class Calculator {
 		);
 	}
 
-	calculate(equals = '') {
+	calculate(equals) {
 		if (this.currentOperator === '*') {
 			this.result = this.multiplyNumbers();
 		} else if (this.currentOperator === '-') {
@@ -44,14 +47,24 @@ export class Calculator {
 		} else if (this.currentOperator === '+') {
 			this.result = this.addNumbers();
 		}
-
-		this.secondOperand.textContent += this.firstOperand.textContent + equals;
-		this.firstOperand.textContent = this.result;
+		if (equals) {
+			this.firstOperand.textContent = this.result;
+			this.clearSecondOperand();
+			this.numberArray = [];
+			this.resultArray.push(this.result);
+		} else {
+			this.resultArray.push(this.result);
+			this.firstOperand.textContent = this.result;
+			this.secondOperand.textContent = this.result + this.currentOperator;
+			this.secondOperand.dataset.secondaryOperand = this.result;
+			this.firstOperand.dataset.primaryOperand = '';
+			this.numberArray = [];
+		}
 	}
 
 	clearFirstOperand() {
-		this.firstOperand.textContent = '0';
-		this.firstOperand.dataset.primaryOperand = '0';
+		this.firstOperand.textContent = '';
+		this.firstOperand.dataset.primaryOperand = '';
 	}
 
 	clearSecondOperand() {
@@ -60,55 +73,52 @@ export class Calculator {
 	}
 
 	clearCalculator() {
-		this.firstOperand.dataset.primaryOperand = '0';
-		this.firstOperand.textContent = '0';
+		this.firstOperand.dataset.primaryOperand = '';
+		this.firstOperand.textContent = '';
 		this.secondOperand.dataset.secondaryOperand = '';
 		this.secondOperand.textContent = '';
-		this.resetOpCount();
+		this.numberArray = [];
 	}
 
 	deleteLastNumber() {
-		let txtHolder = '';
-		if (this.firstOperand.dataset.primaryOperand.length > 1) {
-			txtHolder = this.firstOperand.dataset.primaryOperand.slice(0, -1);
-			this.firstOperand.dataset.primaryOperand = txtHolder;
-			this.firstOperand.textContent = this.firstOperand.dataset.primaryOperand;
-		} else if (
-			this.firstOperand.dataset.primaryOperand.length === 1 &&
-			this.firstOperand.dataset.primaryOperand !== '0'
-		) {
-			this.clearFirstOperand();
+		const arrLength = this.numberArray.length;
+		if (arrLength && arrLength > 0) {
+			this.numberArray.pop();
+			this.firstOperand.dataset.primaryOperand = this.convertArrayToString(
+				this.numberArray
+			);
+			this.firstOperand.textContent = this.convertArrayToString(
+				this.numberArray
+			);
 		}
 	}
 
 	appendNumbers(target) {
-		if (
-			this.firstOperand.textContent === '0' &&
-			this.firstOperand.textContent.length === 1
-		) {
-			this.firstOperand.dataset.primaryOperand = target;
-			this.firstOperand.textContent = this.firstOperand.dataset.primaryOperand;
-		} else if (
-			this.firstOperand.dataset.primaryOperand !== '0' &&
-			this.firstOperand.dataset.primaryOperand.length >= 1
-		) {
-			this.firstOperand.dataset.primaryOperand += target;
-			this.firstOperand.textContent = this.firstOperand.dataset.primaryOperand;
-		}
-		if (this.firstOperand.dataset.primaryOperand.length >= 16) {
-			let strHolder = this.firstOperand.dataset.primaryOperand.slice(0, 16);
-			this.firstOperand.dataset.primaryOperand = strHolder;
-			this.firstOperand.textContent = this.firstOperand.dataset.primaryOperand;
+		this.firstOperand.dataset.primaryOperand += target;
+		this.numberArray.push(target);
+		if (this.numberArray.length < 16) {
+			this.firstOperand.textContent = this.convertArrayToString(
+				this.numberArray
+			);
 		}
 	}
+
 	addOperator(target) {
 		this.currentOperator = target;
-		this.secondOperand.dataset.secondaryOperand =
-			this.firstOperand.dataset.primaryOperand;
+		if (this.regEx.test(this.secondOperand.textContent)) {
+			this.calculate();
+		} else {
+			this.secondOperand.dataset.secondaryOperand = this.convertArrayToString(
+				this.numberArray
+			);
+			this.secondOperand.textContent =
+				this.convertArrayToString(this.numberArray) + this.currentOperator;
+			this.clearFirstOperand();
+			this.numberArray = [];
+		}
+	}
 
-		this.secondOperand.textContent =
-			this.secondOperand.dataset.secondaryOperand + target;
-
-		this.clearFirstOperand();
+	convertArrayToString(arr) {
+		return arr.join('').toString();
 	}
 }
